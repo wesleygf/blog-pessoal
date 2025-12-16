3f67cb803e401e4db766a6b3d39686fb7e0fedcb
wesleygf <wesleygfsilva@hotmail.com>
2025-12-16 00:10:42 -0300

    Deploy do Projeto Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,21 +9,16 @@ import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule]
     }),
     PostagemModule,
     TemaModule,
